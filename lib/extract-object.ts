e732a788118764b646d010fded4593c2f40e0798
Vinicius Marques <vini.type@gmail.com>
2020-07-06 12:38:33 -0300

    refactor(methods): add support to property path

--- lib/extract-object.ts
+++ lib/extract-object.ts
@@ -1,3 +1,5 @@
+import { assocPath } from 'ramda'
+
 import { ClassConstructor } from './types/class-constructor'
 import { Metadata } from './core/metadata'
 import { Hashable } from './types/hashable'
@@ -6,13 +8,12 @@ export function extractObject<T>(
     obj: Hashable & T,
     objKlass: ClassConstructor<T>
 ): object {
-    const resultingObject: Hashable = {}
+    let resultingObject: Hashable = {}
     const propertyMetadata = Metadata.getInstance().findProperties(objKlass)
 
     if (propertyMetadata) {
         for (const { name, propertyKey, type } of propertyMetadata) {
             let value = obj[propertyKey]
-            const resultingProperty = name ?? propertyKey
 
             if (type && value !== undefined) {
                 if (Array.isArray(value)) {
@@ -22,7 +23,13 @@ export function extractObject<T>(
                 }
             }
 
-            resultingObject[resultingProperty] = value
+            const resultingProperty = name ?? propertyKey
+
+            resultingObject = assocPath(
+                resultingProperty.split('.'),
+                value,
+                resultingObject
+            )
         }
     }
 
