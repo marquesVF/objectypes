bbd569c86c5f1579b59a6fdccef2d6c56ff702eb
Vinícius Marques <vini.type@gmail.com>
2020-08-25 12:35:56 -0300

    fix(build-object): build nested object in array

---
+++ test/fixtures/nested-in-array-model.ts
@@ -0,0 +1,35 @@
+import { BuildTransformation, Property, BuildTransformer } from '../../lib'
+
+class DateFormatter implements BuildTransformer<string> {
+
+    transform(value: string): string {
+        return value.replace(/[-]/g, '/')
+    }
+
+}
+
+export class StatementMovement {
+
+    @BuildTransformation(new DateFormatter())
+    @Property({ name: 'date', nullable: true })
+    date?: string
+
+    @BuildTransformation(new DateFormatter())
+    @Property({ name: 'valueDate', nullable: true })
+    valueDate?: string
+
+    @Property()
+    index: number
+
+}
+
+export class Statement {
+
+    @Property({
+        name: 'extractions.data',
+        nullable: true,
+        type: StatementMovement
+    })
+    movement?: StatementMovement[]
+
+}
