09f1341657f0fc79aa5ac8089635266d44a1f9e4
Vinicius Marques <vinicius@foobar.agency>
2021-06-01 23:14:33 +0200

    refactor(map-object): create smaller functions

--- lib/map-object.ts
+++ lib/map-object.ts
@@ -1,12 +1,103 @@
 import { findClassMappingMetadata } from './core/metadata/mapping'
 import { findClassPropertiesMetadata } from './core/metadata/property'
-import { Hashable, ClassConstructor, MapPropertyMetadata } from './types'
+import {
+  Hashable,
+  ClassConstructor,
+  MapPropertyMetadata,
+  PropertyMetadata,
+} from './types'
+
+export function mapObject<T, K>(
+  targetClass: ClassConstructor<Hashable & T>,
+  objectClass: ClassConstructor<K>,
+  obj: Hashable & K
+): T {
+  const targetObj = new targetClass()
+  const mappingMetadata = getMappingMetadata(targetClass, objectClass.name)
+  if (!mappingMetadata) {
+    return targetObj
+  }
+
+  processMappingProperties(mappingMetadata, obj, targetObj)
+  processRegularProperties(mappingMetadata, targetClass, obj, targetObj)
+
+  return targetObj
+}
+
+function getMappingMetadata<T>(
+  targetClass: ClassConstructor<Hashable & T>,
+  objectClassName: string
+) {
+  const mappingMetadata = findClassMappingMetadata(targetClass)
+
+  return mappingMetadata?.filter(
+    property => property.mapTarget.name === objectClassName
+  )
+}
+
+function processMappingProperties<T, K>(
+  mappingProperties: Array<MapPropertyMetadata<Hashable & T, unknown>>,
+  obj: Hashable & K,
+  targetObject: Hashable & T
+) {
+  for (const mappingProperty of mappingProperties) {
+    const value = processValue(mappingProperty, obj)
+
+    Reflect.set(targetObject, mappingProperty.propertyKey, value)
+  }
+}
+
+function processRegularProperties<T, K>(
+  mappingProperties: Array<MapPropertyMetadata<Hashable & T, unknown>>,
+  targetClass: ClassConstructor<Hashable & T>,
+  obj: Hashable & K,
+  targetObject: Hashable & T
+) {
+  const propertyMetadata = findClassPropertiesMetadata(targetClass)
+  if (!propertyMetadata) {
+    return
+  }
+
+  const classProperties = filterNotMappedProperties(
+    mappingProperties,
+    propertyMetadata
+  )
+
+  processProperties(classProperties, targetObject, obj)
+}
+
+function filterNotMappedProperties<T>(
+  mappingProperties: Array<MapPropertyMetadata<Hashable & T, unknown>>,
+  classProperties: PropertyMetadata[]
+) {
+  const mappingKeys = mappingProperties.map(mapping => mapping.propertyKey)
+
+  return classProperties?.filter(classProperty => {
+    if (mappingKeys) {
+      return !mappingKeys.includes(classProperty.propertyKey)
+    }
+
+    return true
+  })
+}
+
+function processProperties<T>(
+  classProperties: PropertyMetadata[],
+  targetObject: Hashable & T,
+  obj: Hashable
+) {
+  for (const classProperty of classProperties) {
+    const { propertyKey } = classProperty
+
+    Reflect.set(targetObject, propertyKey, obj[propertyKey])
+  }
+}
 
 function processValue<T, K>(
-  property: MapPropertyMetadata<T, K>,
+  mapProperty: MapPropertyMetadata<T, K>,
   obj: Hashable
 ): K {
-  const { mapPropertyKey, mapTransformer } = property
+  const { mapPropertyKey, mapTransformer } = mapProperty
 
   if (mapPropertyKey) {
     return obj[mapPropertyKey]
@@ -17,44 +108,4 @@ function processValue<T, K>(
   }
 
   return mapTransformer.transform(obj as T)
-}
-
-export function mapObject<T, K>(
-  targetKlass: ClassConstructor<Hashable & T>,
-  objKlass: ClassConstructor<K>,
-  obj: Hashable & K
-): T {
-  const targetObj = new targetKlass()
-  const mappedProperties = findClassMappingMetadata<T, unknown>(
-    targetKlass
-  )?.filter(property => property.mapTarget.name === objKlass.name)
-
-  // eslint-disable-next-line no-unused-expressions
-  mappedProperties?.forEach(property => {
-    const value = processValue(property, obj)
-
-    Reflect.set(targetObj, property.propertyKey, value)
-  })
-
-  // Get regular properties ignoring mappedProperties
-  const mappedKeys = mappedProperties?.map(mapped => mapped.propertyKey)
-  const properties = findClassPropertiesMetadata(targetKlass)?.filter(
-    property => {
-      if (mappedKeys) {
-        // Avoid duplicated properties
-        return !mappedKeys.includes(property.propertyKey)
-      }
-
-      return true
-    }
-  )
-
-  // eslint-disable-next-line no-unused-expressions
-  properties?.forEach(property => {
-    const { propertyKey } = property
-
-    Reflect.set(targetObj, propertyKey, obj[propertyKey])
-  })
-
-  return targetObj
 }
