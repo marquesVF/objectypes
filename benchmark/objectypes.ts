a6efca707e57f102f8bc53b3890b541cf0da78c3
Vinicius Marques <vinicius@foobar.agency>
2021-11-10 22:14:31 +0100

    refactor: benchmark

--- benchmark/objectypes.ts
+++ benchmark/objectypes.ts
@@ -1,17 +1,10 @@
-/* eslint-disable no-console */
 import { buildObject } from '../lib'
 
 import data from './data.json'
-import {
-  executeMultipleTimes,
-  measureExecutionTimeInMilliseconds,
-} from './helpers'
+import { runBenchmark } from './helpers'
 import { BenchmarkData } from './types/objectypes-object'
 
-const executionTimes = executeMultipleTimes(1000, () =>
-  measureExecutionTimeInMilliseconds(() => buildObject(BenchmarkData, data))
-)
-const sum = executionTimes.reduce((a, b) => a + b, 0)
-const avg = sum / executionTimes.length || 0
+const average = runBenchmark(() => buildObject(BenchmarkData, data))
 
-console.log(`Objectypes average time is ${avg}ms`)
+// eslint-disable-next-line no-console
+console.log(`Objectypes average time is ${average}ms`)
