53db9bb38a5c15e16a30b8e6b759c989761c69da
Vinicius Marques <vini.type@gmail.com>
2021-11-10 22:14:31 +0100

    refactor: benchmark

--- benchmark/helpers.ts
+++ benchmark/helpers.ts
@@ -1,13 +1,22 @@
-export function executeMultipleTimes<T>(executions: number, fn: () => T) {
-  const returns: T[] = []
-  for (let i = 0; i < executions; i++) {
-    returns.push(fn())
-  }
+export function runBenchmark<T>(fn: () => T) {
+  const executionTimes = executeMultipleTimes(10000, () =>
+    measureExecutionTimeInMilliseconds(fn)
+  )
+  const sum = executionTimes.reduce((a, b) => a + b, 0)
 
-  return returns
+  return sum / executionTimes.length
 }
 
-export function measureExecutionTimeInMilliseconds(fn: Function) {
+function executeMultipleTimes<T>(executions: number, fn: () => T) {
+  const executionResults: T[] = []
+  for (let i = 0; i < executions; i++) {
+    executionResults.push(fn())
+  }
+
+  return executionResults
+}
+
+function measureExecutionTimeInMilliseconds(fn: Function) {
   const start = process.hrtime()
   fn()
   const end = process.hrtime(start)
